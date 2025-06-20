e38d1c2225baa4c9a238ce111a1327a30f92a54a
fhevert <fabianhevert@googlemail.com>
2025-06-20 12:44:46 +0200

    Durchschnittspunkte

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {DataSnapshot, onValue, ref} from "firebase/database";
 import {useAuth} from './firebase/AuthContext';
 import {signOut} from 'firebase/auth';
 import Login from "./pages/login";
+import {convertFromDbGame} from "./firebase/DbFunctions";
 
 // Separate component for auth-dependent UI parts
 const AuthStatusBar = memo(() => {
@@ -70,39 +71,6 @@ function App() {
             mode: 'light',
         },
     });
-    // Funktion, um aus einem Array von RoundResult eine Map zu erstellen
-    function resultsArrayToMap(results: {key: string, value: number}[]): Map<string, number> {
-      const resultsMap = new Map<string, number>();
-      for (const result of results) {
-        resultsMap.set(result.key, result.value);
-      }
-      return resultsMap;
-    }
-
-    function convertToRoundsResultsMaps(gameToConvert: any): Game {
-         var rounds: Round[];
-        if(gameToConvert.rounds){
-            rounds= new Array(gameToConvert.rounds.length);
-            var i: number =0
-            for (const round of gameToConvert.rounds) {
-                rounds[i] = {
-                    id: round.id,
-                    roundPoints: round.roundPoints,
-                    cowardicePoints: round.cowardicePoints,
-                    results: resultsArrayToMap(round.results)
-                }as Round
-                i++;
-            }
-        }else{
-            rounds = new Array(0);
-        }
-
-        return {
-            players: JSON.parse(JSON.stringify(gameToConvert.players)),
-            rounds: rounds,
-            averagePoints: gameToConvert.averagePoints,
-        } as Game;
-      }
 
      useEffect(() => {
          const collectionRef = ref(firebaseDB, 'game');
@@ -113,7 +81,7 @@ function App() {
                 if (firebaseDbSnapshot.exists()) {
                     const game = firebaseDbSnapshot.val() as Game
                     if (game) {
-                        setGame(convertToRoundsResultsMaps(game));
+                        setGame(convertFromDbGame(game));
                     }
                }
                setIsLoading(false);
--- src/firebase/DbFunctions.tsx
+++ src/firebase/DbFunctions.tsx
@@ -1,6 +1,7 @@
 import {Game} from "../model/Game";
 import { firebaseDB } from "./firebase-config";
 import {ref, set} from "firebase/database";
+import {Round} from "../model/Round";
 
 export function saveGameToFirebase(game: Game): Promise<void> {
     const gameRef = ref(firebaseDB, 'game'); // Pfad in der Datenbank
@@ -25,4 +26,38 @@ export function saveGameToFirebase(game: Game): Promise<void> {
     }
 
     return set(gameRef, gameToSave);
+}
+
+// Funktion, um aus einem Array von RoundResult eine Map zu erstellen
+function resultsArrayToMap(results: {key: string, value: number}[]): Map<string, number> {
+    const resultsMap = new Map<string, number>();
+    for (const result of results) {
+        resultsMap.set(result.key, result.value);
+    }
+    return resultsMap;
+}
+
+export function convertFromDbGame(gameToConvert: any): Game {
+    var rounds: Round[];
+    if(gameToConvert.rounds){
+        rounds= new Array(gameToConvert.rounds.length);
+        var i: number =0
+        for (const round of gameToConvert.rounds) {
+            rounds[i] = {
+                id: round.id,
+                roundPoints: round.roundPoints,
+                cowardicePoints: round.cowardicePoints,
+                results: resultsArrayToMap(round.results)
+            }as Round
+            i++;
+        }
+    }else{
+        rounds = new Array(0);
+    }
+
+    return {
+        players: JSON.parse(JSON.stringify(gameToConvert.players)),
+        rounds: rounds,
+        averagePoints: gameToConvert.averagePoints,
+    } as Game;
 }
