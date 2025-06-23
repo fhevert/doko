7f378bc5af250e417ea5b5e3da330c89062da6f1
fhevert <fabianhevert@googlemail.com>
2025-06-23 10:03:30 +0200

    Solo und Bock

--- src/pages/result/pointcell/PointCell.tsx
+++ src/pages/result/pointcell/PointCell.tsx
@@ -6,7 +6,7 @@ function PointCell(parameters: { round: Round}) {
     const key = useId();
     return (
         <TableCell sx={{ whiteSpace: 'nowrap', width:'0px', borderRight: '1px solid #e0e0e0'}} align={'center'} className='cellWithRightLine' key={'TC' + key}>
-            <Stack width='100%' alignItems="center" justifyContent="center" direction="row" spacing={1} divider={<Typography>|</Typography>}>
+            <Stack width='100%' alignItems="center" justifyContent="center" direction="row" spacing={'1px'} divider={<Typography>|</Typography>}>
                 {parameters.round.solo && <Typography>{'S'}</Typography>}
                 {parameters.round.bock && <Typography>{'B'}</Typography>}
                 <Typography>{parameters.round.roundPoints}</Typography>
